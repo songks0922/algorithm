970afe82f1c8bae8a53719bc8c6c2b79bd523a6a
songks0922 <skj0922@kookmin.ac.kr>
2021-07-01 22:27:38 +0900

    백준 if문 끝, for문 1번

---
+++ Baekjoon/2884.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int hour, minute;
+    cin >> hour >> minute;
+    minute -= 45;
+    if (minute < 0)
+    {
+        hour--;
+        minute = 60 + minute;
+    }
+
+    hour = hour < 0 ? 23 : hour;
+    
+    cout << hour << " " << minute << endl;
+    return 0;
+}
