767f36fe7e91c28ec05fc335d60c86b3a1693c3e
songks0922 <skj0922@kookmin.ac.kr>
2021-08-20 14:56:26 +0900

    걷기: A+B 풀이

---
+++ Baekjoon/1000.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int num1, num2;
+    cin >> num1 >> num2;
+    cout << num1 + num2 << "\n";
+    return 0;
+}
