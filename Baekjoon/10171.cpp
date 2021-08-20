a866ed76c10addb15ec7497c41dd6a95a1d53641
songks0922 <skj0922@kookmin.ac.kr>
2021-08-20 14:53:56 +0900

    걷기: 고양이 풀이

---
+++ Baekjoon/10171.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cout << "\\    /\\\n" <<" )  ( ')\n" << "(  /  )\n" << " \\(__)|\n";
+    
+    return 0;
+}
