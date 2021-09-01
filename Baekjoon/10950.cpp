e222f60bd73e205410a73c5485da1c8afe5c0559
songks0922 <skj0922@kookmin.ac.kr>
2021-09-01 11:31:40 +0900

    걷기: A+B -3 풀이

---
+++ Baekjoon/10950.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  int T, A, B;
+  cin >> T;
+  for (int i = 0; i < T; i++) {
+    cin >> A >> B;
+    cout << A + B << '\n';
+  }
+  return 0;
+}
