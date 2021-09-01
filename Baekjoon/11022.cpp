3dbb78d5f5d0ee896de3cdb1ca137414a1955a4b
songks0922 <skj0922@kookmin.ac.kr>
2021-09-01 11:48:32 +0900

    걷기: A+B -8 풀이

---
+++ Baekjoon/11022.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  int T, A, B;
+  cin >> T;
+  for (int i = 0; i < T; i++) {
+    cin >> A >> B;
+    cout << "Case #" << i+1 << ": " << A << " + " << B << " = " << A + B << '\n';
+  }
+  return 0;
+}
