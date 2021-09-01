98ee99607866f9e8a54bcd4fb279b3743344fd6f
songks0922 <skj0922@kookmin.ac.kr>
2021-09-01 11:36:59 +0900

    걷기: A+B -5

---
+++ Baekjoon/10952.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  int A, B;
+  while (1) {
+    cin >> A >> B;
+    if(A == 0 && B == 0) break;
+    cout << A + B << '\n';
+  }
+  return 0;
+}
