cf1b7274a9c985af2c24a96e5f61634991312533
songks0922 <skj0922@kookmin.ac.kr>
2021-09-18 18:21:52 +0900

    feat: 동전 0 다른사람 풀이 주석 추가

--- Baekjoon/11047.cpp
+++ Baekjoon/11047.cpp
@@ -29,3 +29,25 @@ int main() {
   cout << cnt << '\n';
   return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     int n, k, x, num = 0;
+//     vector<int> v;
+//     cin >> n >> k;
+//     for (int i = 0; i < n; i++) {
+//         cin >> x;
+//         v.push_back(x);
+//     }
+//     for (int i = v.size() - 1; i >= 0; i--) {
+//         if (k == 0)break;
+//         if (v[i] <= k) {
+//             num += k / v[i];
+//             k %= v[i];
+//         }
+//     }
+//     cout << num << '\n';
+// }
