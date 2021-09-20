9436bc0d7b843a7c78202f9e05b74d523fae46f4
songks0922 <skj0922@kookmin.ac.kr>
2021-09-20 12:30:41 +0900

    달리기: 별찍기 8 풀이

---
+++ Baekjoon/2445.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  int n;
+  cin >> n;
+
+  for (int i = n; i > 0; i--) {
+    for (int j = 0; j < n - i + 1; j++) {
+      cout << '*';
+    }
+    for (int j = 0; j < 2 * i - 2; j++) {
+      cout << ' ';
+    }
+    for (int j = 0; j < n - i + 1; j++) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
+
+  for (int i = n-1; i > 0; i--) {
+    for (int j = 0; j < i; j++) {
+      cout << '*';
+    }
+    for (int j = 0; j < (n - i) * 2; j++) {
+      cout << ' ';
+    }
+    for (int j = 0; j < i; j++) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
+  
+  return 0;
+}
