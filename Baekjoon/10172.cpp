7bd210fe3cfb903284adcd5b99f854bebc1082b9
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-10-08 21:16:33 +0900

    feat: 쉬운문제

---
+++ Baekjoon/10172.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  string s = "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n";
+  cout << s << '\n';
+  return 0;
+}
