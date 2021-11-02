2d63723fcb2fb765e5884ba32932af38492bb5b1
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-11-02 13:31:12 +0900

    달리기: 스네이크버드 풀이 java

---
+++ java_algoritm/src/Main_16435.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_16435 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int snakeLen = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] fruits = new int[n];
+
+        for (int i  = 0; i < n; i++) {
+            fruits[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(fruits);
+
+        for (int fruit : fruits) {
+            if (fruit <= snakeLen) snakeLen++;
+            else break;
+        }
+
+        System.out.println(snakeLen);
+
+    }
+
+}
