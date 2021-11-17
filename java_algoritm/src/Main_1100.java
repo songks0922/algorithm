e83cac3ed30218f877114f913ac0ace55100107f
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-11-17 21:19:14 +0900

    feat: 하얀칸 풀이

---
+++ java_algoritm/src/Main_1100.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main_1100 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] Board = new char[8][8];
+        String s = "";
+        int ans = 0;
+
+        for (int i = 0; i < 8; i++) {
+            s = br.readLine();
+            for (int j = 0; j < 8; j++) {
+                Board[i][j] = s.charAt(j);
+                if ( (i + j) % 2 == 0 && Board[i][j] == 'F') ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
