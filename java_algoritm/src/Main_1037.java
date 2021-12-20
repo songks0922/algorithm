09b97ec6275d92f9bbdd0fed1198002b00a63aa3
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-12-20 22:49:20 +0900

    feat: 약수 풀이 자바

---
+++ java_algoritm/src/Main_1037.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main_1037 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        while(T-- > 0) {
+            int N = Integer.parseInt(st.nextToken());
+            max = N > max ? N : max;
+            min = N < min ? N : min;
+        }
+        System.out.println(max * min);
+    }
+}
