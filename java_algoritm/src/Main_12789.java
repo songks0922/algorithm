5ea726bdf5291a82d6b91355d6a6f6d0a318adae
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-01-15 23:25:54 +0900

    feat: 간식드리미 자바

--- java_algoritm/src/Main_12789.java
+++ java_algoritm/src/Main_12789.java
@@ -1,2 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class Main_12789 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        Stack<Integer> s = new Stack<>();
+        int seq = 1;
+        int[] students = new int[N];
+        for (int i = 0; i < N; i++) {
+            students[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (students[i] != seq) {
+                if (!s.isEmpty() && s.peek() == seq) {
+                    s.pop();
+                    i--;
+                    seq++;
+                } else {
+                    s.push(students[i]);
+                }
+            } else {
+                seq++;
+            }
+        }
+
+        while (true) {
+            if (!s.isEmpty() && seq == s.peek()) {
+                s.pop();
+                seq++;
+            } else {
+                break;
+            }
+        }
+
+        if (s.isEmpty()) {
+            System.out.println("Nice");
+        } else {
+            System.out.println("Sad");
+        }
+
+    }
+
 }
