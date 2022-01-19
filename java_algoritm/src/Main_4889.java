58094a1a63ece1e272c1ab03f07b65190fd362dc
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-01-19 20:55:04 +0900

    feat: 안정적인 문자열 자바

---
+++ java_algoritm/src/Main_4889.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main_4889 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int seq = 1;
+        while (true) {
+
+            String s = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            if (s.contains("-")) break;
+            int cnt = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char tmp = s.charAt(i);
+                if (tmp == '{') {
+                    stack.push(tmp);
+                } else {
+                    if (stack.isEmpty()) {
+                        cnt++;
+                        stack.push('{');
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+            sb.append(seq++).append(". " + (cnt + stack.size() / 2));
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
