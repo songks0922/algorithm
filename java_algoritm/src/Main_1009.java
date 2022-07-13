509c96db648f728e2032c05906a5956cf5a457ed
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-07-13 22:21:58 +0900

    feat: 백준 1009 분산처리 자바 풀이

---
+++ java_algoritm/src/Main_1009.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int r=1;
+
+            for (int j = 0; j < b; j++) {
+                r=(r*a)%10;
+            }
+
+            System.out.println((r == 0 ? 10 : r));
+        }
+
+    }
+}
