3b455043ec3d7aaa6d52bf87721b3d6e7aa58989
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-08-09 00:52:03 +0900

    feat: 진약수의 합

---
+++ inflearn/p3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, sum = 1;
+	
+	cin >> n;
+	
+	cout << "1";
+	
+	for (int i = 2; i <n; i++) {
+		if (n % i == 0) {
+			cout << " + " << i;
+			sum += i;
+		}
+	}
+	
+	cout << " = " << sum << endl;
+	
+	return 0;
+}
