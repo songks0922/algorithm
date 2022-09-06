1bf523c62fd8ccef13283f90d208a8e152e39147
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-09-06 20:08:02 +0900

    feat: 아나그램

---
+++ inflearn/p16.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int buffA[60], buffB[60];
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  char str[100];
+
+  cin >> str;
+
+  for (int i = 0; str[i] != '\0'; i++) {
+    if (str[i] >= 65 && str[i] <= 90) {
+      buffA[str[i] - 64]++;
+    } else {
+      buffA[str[i]-70]++;
+    }
+  }
+
+  cin >> str;
+
+  for (int i = 0; str[i] != '\0'; i++) {
+    if (str[i] >= 65 && str[i] <= 90) {
+      buffB[str[i] - 64]++;
+    } else {
+      buffB[str[i]-70]++;
+    }
+  }
+
+  for (int i = 0; i < 52; i++) {
+    if (buffA[i] != buffB[i]) {
+      cout << "NO\n";
+      exit(0);
+    }
+  }
+
+  cout << "YES\n";
+
+  return 0;
+}
