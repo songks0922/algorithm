c61933d06f2cb97ec388821731d24d1f4f90e078
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-10-04 17:22:00 +0900

    feat: isbn

---
+++ Baekjoon/6810.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << "The 1-3-sum is " << 91 + a * 1 + b * 3 + c * 1;
+  return 0;
+}
