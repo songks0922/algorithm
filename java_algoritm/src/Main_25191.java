e1900912cff386f1e5e55bff8f638d5d6e166153
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-10 20:42:06 +0900

    feat: 레벨테스트 풀이

---
+++ java_algoritm/src/Main_25191.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_25191 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int count = A / 2 + B;
+
+        if (count > N) {
+            System.out.println(N);
+            return;
+        }
+
+        System.out.println(count);
+
+    }
+}
---
+++ java_algoritm/src/Main_4999.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_4999 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String jehawn = br.readLine();
+        String doctor = br.readLine();
+
+        if (jehawn.length() < doctor.length()) {
+            System.out.println("no");
+            return;
+        }
+
+        System.out.println("go");
+    }
+}
