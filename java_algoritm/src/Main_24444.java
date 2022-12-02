fcab239d3915a569395aa460c6b0363e48458f66
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-12-02 11:21:13 +0900

    feat: bfs 문제 풀이들

--- java_algoritm/src/Main_24444.java
+++ java_algoritm/src/Main_24444.java
@@ -1,31 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+import java.util.Queue;
 
-public class Dfa {
+class Main_24444{
+    static ArrayList<Integer>[] adjacent;
+    static int users;
 
-    static int[][] dfa;
-    static void constructDfa(String pattern) {
-        int patLength = pattern.length();
-        dfa[pattern]
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < t; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String pattern = st.nextToken();
-            String text = st.nextToken();
-
-            dfa = new int[3][7];
-
-
+    public static int addCnt(int[] visited) {
+        int sum = 0;
+        for(int e : visited) {
+            //System.out.print(e+" ");
+            sum+=e;
         }
+        //System.out.println(sum);
+        return sum;
     }
+    public static int bfs(int start) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        int[] visited = new int[users+1];
+        boolean[] marked = new boolean[users+1];
+        queue.add(start);
+        marked[start] = true;
+        while(!queue.isEmpty()) {
+            int first = queue.poll();
+            for(int e : adjacent[first]) {
+                if(visited[e]==0 && !marked[e]) {
+                    queue.offer(e);
+                    marked[e] = true;
 
+                    visited[e]=visited[first]+1;
+                }
+            }
+        }
+
+        return addCnt(visited);
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] info = br.readLine().split("\\s");
+        users = Integer.parseInt(info[0]);
+        int relations = Integer.parseInt(info[1]);
+        adjacent = new ArrayList[users+1];
+
+        for(int i=1; i<=users; i++) {
+            adjacent[i] = new ArrayList<Integer>();
+        }
+        for(int i=0; i<relations ; i++) {
+            String[] arr = br.readLine().split("\\s");
+            int a = Integer.parseInt(arr[0]);
+            int b = Integer.parseInt(arr[1]);
+
+            adjacent[a].add(b);
+            adjacent[b].add(a);
+        }
+
+        int answer = 1000000;
+        int answerUser = 0;
+        for(int i=1 ; i<=users; i++) {
+            int a = bfs(i);
+            if(a<answer) {
+                answer = a;
+                answerUser = i;
+            }
+        }
+
+        System.out.println(answerUser);
+    }
 }
