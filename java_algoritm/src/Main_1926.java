500dd1a116f2e15fb4535e9f81a69c6a194e4a73
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-12-06 09:30:18 +0900

    feat: 1926 그림 bfs 풀이

---
+++ java_algoritm/src/Main_1926.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_1926 {
+
+    static int n, m;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    static int[][] map;
+    static boolean[][] visited;
+    static int max = 0;
+
+    static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static void bfs(int x, int y) {
+        Queue<Node> queue = new LinkedList<>();
+        visited[x][y] = true;
+        queue.add(new Node(x, y));
+
+        int count = 1;
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (!visited[nx][ny] && map[nx][ny] == 1) {
+                        queue.add(new Node(nx, ny));
+                        visited[nx][ny] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        if (max < count) {
+            max = count;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int total = 0;
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && map[i][j] == 1) {
+                    bfs(i, j);
+                    total++;
+                }
+            }
+        }
+
+        System.out.print(String.format("%d\n%d", total, max));
+    }
+}
