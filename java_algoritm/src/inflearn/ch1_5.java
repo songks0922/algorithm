19a45151327a5ee2a30aeac8a826dbea32724aa8
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-19 18:01:55 +0900

    feat: 특정 단어 뒤집기 인프런 강의 풀이

---
+++ java_algoritm/src/inflearn/ch1_5.java
@@ -0,0 +1,46 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ch1_5 {
+
+    private static String solution(String str) {
+        char[] list = str.toCharArray();
+
+        return reverseString(list, 0, list.length - 1);
+    }
+
+    private static String reverseString(char[] str, int start, int end) {
+        while (start < end) {
+            if (Character.isAlphabetic(str[start]) && Character.isAlphabetic(str[end])) {
+                char temp = str[start];
+                str[start] = str[end];
+                str[end] = temp;
+
+                start++;
+                end--;
+                continue;
+            }
+            if (!Character.isAlphabetic(str[start])) {
+                start++;
+            }
+            if (!Character.isAlphabetic(str[end])) {
+                end--;
+            }
+        }
+
+        return String.valueOf(str);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(solution(str));
+    }
+
+
+}
