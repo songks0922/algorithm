74aec80c374105b6ebd6fc1e17242161857e5ec5
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-20 17:33:40 +0900

    feat: 중복 문자 제거 인프런 강의 풀이

---
+++ java_algoritm/src/inflearn/ch1_6.java
@@ -0,0 +1,28 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ch1_6 {
+
+    private static String solution(String str) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i == str.indexOf(str.charAt(i))) answer.append(str.charAt(i));
+        }
+
+        return answer.toString();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(solution(str));
+
+    }
+}
