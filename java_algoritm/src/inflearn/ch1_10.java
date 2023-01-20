08e646151d1435ad2939d7a6ac84810cdebab964
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-20 19:24:12 +0900

    feat: 문자 거리 인프런 강의 풀이

---
+++ java_algoritm/src/inflearn/ch1_10.java
@@ -0,0 +1,57 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ch1_10 {
+
+    private static String solution(String input) {
+        StringTokenizer st = new StringTokenizer(input);
+        String str = st.nextToken();
+        char target = st.nextToken().charAt(0);
+        int[] answer = new int[101];
+        int p = 1000;
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            if (str.charAt(i) != target) {
+                p++;
+                answer[i] = p;
+            }
+            if (str.charAt(i) == target) {
+                p = 0;
+                answer[i] = p;
+            }
+        }
+        p = 1000;
+
+        for (int i = len - 1; i >= 0; i--) {
+            if (str.charAt(i) != target) {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+
+            if (str.charAt(i) == target) {
+                p = 0;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(answer[i]).append(" ");
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        System.out.println(solution(input));
+    }
+
+}
