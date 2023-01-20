8b53e5832b3d3a5474f20402a45d74e8ed2609a1
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-20 20:36:47 +0900

    feat: 암호 인프런 강의 풀이

---
+++ java_algoritm/src/inflearn/ch1_12.java
@@ -0,0 +1,51 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ch1_12 {
+
+    private static String solution(String str) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < str.length() - 1; i += 7) {
+            answer.append(convert(str.substring(i, i + 7)));
+        }
+
+        return answer.toString();
+    }
+
+    private static char convert(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (char x : str.toCharArray()) {
+            if (x == '#') {
+                result.append("1");
+            }
+            if (x == '*') {
+                result.append("0");
+            }
+        }
+        return (char) Integer.parseInt(result.toString(), 2);
+    }
+
+    private static String solution2(int n, String str) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            String temp = str.substring(0, 7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(temp, 2);
+            answer.append((char) num);
+            str = str.substring(7);
+        }
+        return answer.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+//        System.out.println(solution(br.readLine()));
+        System.out.println(solution2(N, br.readLine()));
+    }
+}
