1699af71b49cb217f811f179ee64837780c6143b
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-01-24 21:28:13 +0900

    feat: 1247 부호 풀이 (BigInteger 이용)

---
+++ java_algoritm/src/boj/Main_1247.java
@@ -0,0 +1,36 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main_1247 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            int N = Integer.parseInt(br.readLine());
+
+            BigInteger sum = new BigInteger("0");
+
+            for (int j = 0; j < N; j++) {
+                BigInteger num = new BigInteger(br.readLine());
+                sum = sum.add(num);
+            }
+
+            if (sum.compareTo(BigInteger.ZERO) == -1) {
+                answer.append("-").append("\n");
+            }
+            if (sum.compareTo(BigInteger.ZERO) == 1) {
+                answer.append("+").append("\n");
+            }
+            if (sum.compareTo(BigInteger.ZERO) == 0) {
+                answer.append("0").append("\n");
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
